d68bb97e10ce7b19729395a40118d9faf2e38793
Ubuntu <dgreen17@gmail.com>
2017-12-07 08:56:38 +0000

    Purging erb files

--- app/models/province.rb
+++ app/models/province.rb
@@ -3,5 +3,5 @@ class Province < ApplicationRecord
   validates :name, :short, presence: true
   validates :name, length: { minimum: 3 }
   validates :short, length: { maximum: 3 }
-  validates :gst, :hst, :pst, numericality: { greater_than: 0, less_than: 30 }
+  validates :gst, :hst, :pst, numericality: { greater_than_or_equal: 0, less_than: 30 }
 end
