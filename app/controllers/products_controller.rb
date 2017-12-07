79ccf7d0cfda8ac87a93abe8c8b6b174011e38c9
Ubuntu <dgreen17@gmail.com>
2017-12-07 09:54:12 +0000

    Added basic search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,9 @@
 class ProductsController < ApplicationController
   def index
-    @servers = ServerSpec.all.order(:bill_rate).paginate(:page => params[:page], :per_page => 4)
+    @servers = if params[:term]
+      ServerSpec.where('name LIKE ? OR description LIKE ?', "%#{params[:term]}%", "%#{params[:term]}%").order(:bill_rate).paginate(:page => params[:page], :per_page => 4)
+    else
+      @servers = ServerSpec.all.order(:bill_rate).paginate(:page => params[:page], :per_page => 4)
+    end
   end
 end
